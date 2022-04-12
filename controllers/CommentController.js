c9c2bc24522c11014c364b5c1ca9e77dfdaa8c0e
TakKwan <waslf3651@gmail.com>
2022-04-12 11:35:40 -0400

    done

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,7 +1,69 @@
 const { Comment, User } = require('../models')
 
 // Work Here
+
+const GetAllComments = async (req, res) => {
+  try {
+    const allComments = await Comment.findAll()
+    res.send(allComments)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetRecentComments = async (req, res) => {
+  try {
+    const recents = await Comment.findAll({ order: [['created_at', 'DESC']] })
+    res.send(recents)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreatetComment = async (req, res) => {
+  try {
+    let ownerId = parseInt(req.query.user_id)
+    let twertId = parseInt(req.params.twert_id)
+    let commentBody = {
+      ownerId,
+      twertId,
+      ...req.body
+    }
+    let comment = await Comment.create(commentBody)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    let updatedComment = await Comment.update(req.body, { where: { id: commentId }, returning: true })
+    res.send(updatedComment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    await Comment.destroy({ where: { id: commentId } })
+    res.send({ message: `deleted with id of ${commentId}` })
+  } catch (error) {
+    throw error
+  }
+}
+
+
 // Work Here
 
 // Dont forget to export your functions
-module.exports = {}
+module.exports = {
+  CreatetComment,
+  GetRecentComments,
+  GetAllComments,
+  UpdateComment,
+  DeleteComment
+}
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,6 +1,12 @@
 const Router = require('express').Router()
+const controller = require('../controllers/CommentController')
+
 
 // You Do
 // Implement Crud Operations For Comments
+Router.get('/recents', controller.GetRecentComments)
+Router.post('/:twert_id', controller.CreatetComment)
+Router.put('/:comment_id', controller.UpdateComment)
+Router.delete('/:comment_id', controller.DeleteComment)
 // You Do
 module.exports = Router
